5d664f3b99d995fd0840117b93beff9f919df6e4
atharmon <atharmon@tamu.edu>
2022-08-29 09:20:53 -0500

    Added dice images on roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +11,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val resultTextView = findViewById<TextView>(R.id.textView)
+        val diceImageView = findViewById<ImageView>(R.id.imageView)
+        diceImageView.setImageResource(R.drawable.dice_1)
+
         val rollButton = findViewById<Button>(R.id.button)
         val dice = Dice(6)
 
         rollButton.setOnClickListener {
-            resultTextView.text = dice.roll().toString()
+            val result = dice.roll()
+            diceImageView.contentDescription = result.toString()
+
+            val imageResource = when (result) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> R.drawable.dice_6
+            }
+            diceImageView.setImageResource(imageResource)
         }
+
+
     }
 }
 

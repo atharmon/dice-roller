3393fa238ff68590c9ef712ce1e0b31d51c3646e
atharmon <atharmon@tamu.edu>
2022-08-29 00:45:26 -0500

    Added button clickListener to change resultTextView text

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val resultTextView = findViewById<TextView>(R.id.textView)
+        val rollButton = findViewById<Button>(R.id.button)
+
+        rollButton.setOnClickListener {
+            resultTextView.text = "6"
+        }
     }
 }
